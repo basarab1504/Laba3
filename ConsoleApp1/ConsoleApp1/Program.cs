8f3470f2e85d16ab4fd54c3f05a1958b2c1882c1
PlatonKlb <57142723+PlatonKlb@users.noreply.github.com>
2019-11-01 18:47:42 +0300

    Добавлено меню

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,6 +15,11 @@ namespace ConsoleApp1
         public string course;
         public string group;
         public float averagePoints;
+
+        public void Print()
+        {
+            Console.WriteLine($"ФИО: {name} Дата рождения: {dateOfBirth} Институт: {instute} Курс: {course} Группа: {group} Средний балл: {averagePoints}");
+        }
     }
 
     public class DB
@@ -22,44 +27,90 @@ namespace ConsoleApp1
         public List<Student> students = new List<Student>();
 
         public void Menu()
+        {
+            string input = "";
+
+            Console.WriteLine("\tЗдравствуйте" +
+                "\nВыберите функцию:" +
+                "\nВывести список студентов" +
+                "\nДобавление студентов" +
+                "\nМодификация списка студентов" +
+                "\nУдаление студентов из списка" +
+                "\nСортировка по ФИО" +
+                "\nСортировка по дате" +
+                "\nОбратная сортировка по ФИО" +
+                "\nОбратная сортировка по дате" +
+                "\nПоиск студентов по ФИО" +
+                "\nПоиск студентов по дате рождения" +
+                "\nНахождение максимального среднего балла" +
+                "\nНахождение минимального среднего балла" +
+                "\nНахождение среднего балла" +
+                "\nНахождение суммы средних баллов");
+
+            while(input != "stop")
+            {
+                input = Console.ReadLine();
+
+                switch(input)
+                {
+                    case "1":
+                        Print();
+                        break;
+                }
+            }
+        }
+
+        public void Add(List<Student> students)
         { }
 
-        public void Add()
+        public void Modify(List<Student> students)
         { }
 
-        public void Modify()
+        public void Delete(List<Student> students)
         { }
 
-        public void Delete()
-        { }
-
-        public void Sort()
-        { }
-
-        public void ReverseSort()
-        { }
+        public void Sort(Comparer<Student> comparer)
+        {
+            students.Sort(comparer);
+        }
 
         public void Search()
-        { }
+        { 
+        
+        }
 
         public void Average()
         { }
 
         public void Sum()
         { }
+
+        public void SaveToFile()
+        { }
+
+        void Print()
+        {
+            foreach (Student s in students)
+                s.Print();
+        }
     }
 
-    public static class InputHandle
+    public static class InputHandleService
     {
-        public static Student KeyboardInput()
+        public static List<Student> InputHandle()
         {
-
+            return null;
         }
 
-        public static Student FileInput()
-        {
+        //static List<Student> KeyboardInput()
+        //{
 
-        }
+        //}
+
+        //static List<Student> FileInput()
+        //{
+
+        //}
     }
 
     class Program
@@ -67,9 +118,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             DB dataBase = new DB();
+            dataBase.students = students;
+            dataBase.Menu();
         }
 
-        public List<Student> sutdents = new List<Student>()
+        public static List<Student> students = new List<Student>()
             {
                 new Student()
                 {
